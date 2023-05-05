e1c9ff7428bcbcfbd16ea594c8dc638dceb824fc
Synel <montagna.samuel@gmail.com>
2023-05-05 14:31:17 +0200

    version change

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from data.lib import *
 
     # Class
 class Application(QBaseApplication):
-    BUILD = '07e755f6'
+    BUILD = '07e76a29'
     VERSION = 'Experimental'
 
     SERVER_NAME = 'OgeNext'
--- updater.py
+++ updater.py
@@ -18,7 +18,7 @@ from data.lib.widgets.updater import data as updater_data
 
     # Class
 class QUpdater(QBaseApplication):
-    BUILD = '07e755f6'
+    BUILD = '07e76a29'
     VERSION = 'Experimental'
 
     UPDATE_LINK = 'https://github.com/Synell/OGE-Next'
