1baf47be2578584ddad273c8e57901cd54e3eee8
Synel <70210528+Synell@users.noreply.github.com>
2023-09-21 17:08:56 +0200

    version change

--- data/lib/globalinfo/Info.py
+++ data/lib/globalinfo/Info.py
@@ -10,7 +10,7 @@ class Info:
     def __new__(cls) -> None:
         return None
 
-    build: str = '07e7bf10'
+    build: str = '07e7bfac'
     version: str = 'Experimental'
 
     application_name: str = 'Oge Next'
